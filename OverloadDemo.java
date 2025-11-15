10cf97c9e796a30b6750ef298f97e386349b26a4
Hariss-12 <24itr031@gmail.com>
2025-11-15 10:31:50 +0530

    Add project files

---
+++ OverloadDemo.java
@@ -0,0 +1,21 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+    double add(double a, double b) {
+        return a + b;
+    }
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+public class OverloadDemo {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+
+        System.out.println(c.add(10, 20));          
+        System.out.println(c.add(2.5, 3.7));        
+        System.out.println(c.add(1, 2, 3));         
+    }
+}
